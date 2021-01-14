c5c356c221fced005accc07b7b8cfe02605af42d
Saquib <skzaidionline89@gmail.com>
2021-01-14 16:37:55 +0530

    writer api integrated

--- src/app/actors/actors.component.ts
+++ src/app/actors/actors.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
 
 @Component({
   selector: 'app-actors',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./actors.component.css']
 })
 export class ActorsComponent implements OnInit {
+  actordata:any=[];
+  forImageUrl: string='https://checkfast.herokuapp.com';
 
-  constructor() { }
+  constructor(public service:ApiService) { }
 
   ngOnInit() {
+    this.getActorData()
+  }
+
+
+  getActorData(){
+    this.service.getApi('actor/actors/?page=0&size=50',0).subscribe((res:any)=>{
+      console.log("------>>>>",res)
+      this.actordata=res.items;
+      console.log("actordata===>>>>>4",res)
+    })
   }
 
 }
--- src/app/writer/writer.component.ts
+++ src/app/writer/writer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
 
 @Component({
   selector: 'app-writer',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./writer.component.css']
 })
 export class WriterComponent implements OnInit {
+  writerdata:any=[];
+  forImageUrl: string='https://checkfast.herokuapp.com';
 
-  constructor() { }
+  constructor(public service:ApiService) { }
 
   ngOnInit() {
+    this.getWriterData()
+  }
+
+  getWriterData(){
+    this.service.getApi('writer/writers/?page=0&size=50',0).subscribe((res:any)=>{
+      console.log("------>>>>",res)
+      this.writerdata=res.items;
+      console.log("writerdata===>>>>>4",res)
+    })
   }
 
 }
